61817fbb31b5049200d6812b7f7b6f5f97587636
Joedson Ferreira <95484503+devjoedson91@users.noreply.github.com>
2024-07-03 22:53:08 -0300

    feat - testing functions

--- src/utils/__tests__/functions.spec.ts
+++ src/utils/__tests__/functions.spec.ts
@@ -1,4 +1,25 @@
-import { searchByKeyword } from "../functions";
+import { VideoProps } from "../../components/video-item";
+import { searchByKeyword, updateListVideos } from "../functions";
+
+document.body.innerHTML = `
+  <div id="app">
+    <main class="mf-videos">
+    <header>
+      <h1>MF Videos</h1>
+      <div class="search-content">
+        <input type="text" placeholder="Pesquisar" class="search-input" />
+        <button class="btn-search">
+          <i class="ph ph-magnifying-glass"></i>
+        </button>
+      </div>
+      <button class="btn-drawer-menu">
+        <i class="ph ph-list"></i>
+      </button>
+    </header>
+    <div class="video-list"></div>
+    </main>
+  </div>
+`;
 
 describe("functions tests", () => {
   it("searchByKeyword should be called with a string param", () => {
@@ -7,5 +28,29 @@ describe("functions tests", () => {
     fn("javascript");
 
     expect(typeof fn.mock.calls[0][0]).toBe("string");
+  });
+
+  it("updateListVideos should be called with a list array param", () => {
+    const fn = jest.fn(updateListVideos);
+
+    const list: VideoProps[] = [
+      {
+        id: {
+          videoId: "lkIFF4maKMU",
+        },
+        snippet: {
+          title: "100+ JavaScript Concepts you Need to Know",
+          thumbnails: {
+            medium: {
+              url: "https://i.ytimg.com/vi/lkIFF4maKMU/mqdefault.jpg",
+            },
+          },
+        },
+      },
+    ];
+
+    fn(list);
+
+    expect(typeof fn.mock.calls[0][0]).toBe("object");
   });
 });
